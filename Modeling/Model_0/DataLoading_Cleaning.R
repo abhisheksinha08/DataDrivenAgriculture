5cae62d1c1f6413aedc2185f40b13d723d6d6599
Abhishek Sinha <abhishek.sparta@gmail.com>
2017-12-07 21:33:12 +0530

    Cleaned Yield Data
    Cleaned yield data, fixed crop names, fixed block names.

--- Modeling/Model_0/DataLoading_Cleaning.R
+++ Modeling/Model_0/DataLoading_Cleaning.R
@@ -12,6 +12,26 @@ yield_db <- as.data.frame(readxl::read_xlsx("Yield//Tidy_Data_1.xlsx", sheet = 1
 table(yield_db$Crop)
 
 yield_db$Crop[yield_db$Crop=='Rice'] <-'Paddy'
+yield_db$Crop[yield_db$Crop=='Rabi oil seed'] <-'Rabi Oil Seed'
 
-table(yield_db$District)
+#Remove Rabi Oil Seed
+yield_db <- yield_db[yield_db$Crop!='Rabi Oil Seed',]
 
+head(yield_db)
+
+
+table(yield_db$Block)[table(yield_db$Block)<35]
+
+## Fix Block Names
+
+incorrect_names <- c("B/Garh","B/khera","Badhara","Bapouli","Dadri- I","F.P./Zhorka","F/Nagar","Gharounda","Hodel","L/Majra","Ladawa","Madlouda","Nathusiri","Patoudi","Rojound","Sahalawas","Sewan","Taoru","N/Chaudhary","Hasanpur")
+correct_names <-c("Bahadurgarh","Bawani Khera","Badhra","Bapoli","Dadri-I","Ferozpur Jhirka","Farrukh Nagar","Gharaunda","Hodal","Lakhan Majra","Ladwa","Matlauda","Nathusari","Pataudi","Rajound","Salhawas","Siwan","Tauru","Nangal Chaudhary","Hassanpur")
+
+i<-1
+for (inc_name in incorrect_names) {
+    yield_db$Block[yield_db$Block==inc_name] <- correct_names[i]
+    i<-i+1
+}
+
+
+write.csv(yield_db, "Tidy_Data_final.csv", row.names = F)

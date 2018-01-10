d86428d3226260272813ae3dfa689062ec9ec890
Abhishek Sinha <abhishek.sparta@gmail.com>
2018-01-10 21:50:36 +0530

    Aggregate Soil Nutrient By Block and Year

--- SoilNutrientDataExtracter/Soil_Nutrient_Year_Block_Summarization.R
+++ SoilNutrientDataExtracter/Soil_Nutrient_Year_Block_Summarization.R
@@ -1,4 +1,5 @@
 library(data.table)
+library(dplyr)
 
 
 soil_df <- fread("Soil_Nutrient_Year.csv", header = T, stringsAsFactors = F, showProgress = T)
@@ -8,22 +9,37 @@ str(soil_df)
 soil_df$V1 <- NULL
 
 
-table(soil_df$Year)
-
-
 soil_df$Year <- as.integer(soil_df$Year)
 ################################
 # Fixing Data on the basis of EDA - Soil_Nutrient_EDA1 (http://rpubs.com/abhishek08/348055)
 ################################
 
-###Soil PH
-
-table(is.na(soil_df$SoilPh)) #6319 is missing (<1%)
-table(soil_df$SoilPh>14) # 1652 records have incorrect data (<1%)
 # Fix PH assigning mean to any value that is above 14 or is null
 soil_df$SoilPh[soil_df$SoilPh>14] <- mean(soil_df$SoilPh[soil_df$SoilPh<=14], na.rm = T)
-soil_df$SoilPh[is.na(soil_df$SoilPh)] <- mean(soil_df$SoilPh[soil_df$SoilPh<=14], na.rm = T)
+
+cols_to_drop <- c("Nitrogen","Copper","Boron")
+soil_df_selected <- soil_df %>% select(-one_of(cols_to_drop))
+soil_df_selected <- as.data.frame(soil_df_selected)
 
 
+# Insert mean value in missing data
+mean_rm_na <- function(x)
+{
+    x[is.na(x)] <- mean(x, na.rm = T)
+    return(x)
+}
 
-###Electrical Conductivty
+soil_df_selected[,5:14] <- as.data.frame(sapply(soil_df_selected[,5:14], mean_rm_na))
+
+str(soil_df_selected)
+
+soil_summarized <- soil_df_selected %>% group_by(DistrictId, DistrictName, BlockId, BlockName, Year) %>% 
+    summarize(SoilPh = mean(as.numeric(SoilPh), na.rm = T),
+              ElectricalConductivity = mean(ElectricalConductivity), OrganicCarbon=mean(OrganicCarbon),
+                  Phosphorous = mean(Phosphorous), Potassium = mean(Potassium), Sulphur = mean(Sulphur), Zinc = mean(Zinc),
+                  Iron = mean(Iron), Magnesium = mean(Magnesium))
+soil_summarized <- as.data.frame(soil_summarized)
+
+str(soil_summarized)
+
+write.csv(soil_summarized, "Soil_Nutrient_Summarized.csv", row.names = F, col.names = T)

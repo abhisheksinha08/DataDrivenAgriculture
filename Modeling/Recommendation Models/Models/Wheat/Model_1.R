640d81fe67efb13d7b4f9d44ed11ace7367c7374
Madhup Kumar <madhupkumar@gmail.com>
2018-01-14 20:15:09 +0530

    Created new data.frame R vars for train,validation,test to contain data for features only sans outcome

--- Modeling/Recommendation Models/Models/Wheat/Model_1.R
+++ Modeling/Recommendation Models/Models/Wheat/Model_1.R
@@ -18,6 +18,16 @@ index <- createDataPartition(test_df$Yield, p = 0.5, list = F)
 valid_df <- test_df[index,]
 test_df <- test_df[-index,]
 
+# Outcome vectors for Train, Validation and Test
+y_train <- train_df$Yield
+y_valid <- valid_df$Yield
+y_test <- test_df$Yield
+
+# Train, Validation and Test Data frames with only the Features
+train_feat_df <- select(train_df,-Yield)
+valid_feat_df <- select(valid_df,-Yield)
+test_feat_df <- select(test_df,-Yield)
+
 
 #RMSE Function
 rmse <- function(x, y){
@@ -33,25 +43,19 @@ rp1 <- train(Yield ~ ., data = train_df, method = "rpart")
 rmse(predict(rp1), y_train)
 rmse(predict(rp1, valid_df), y_valid)
 
-y_train <- train_df$Yield
-y_valid <- valid_df$Yield
-y_test <- test_df$Yield
 
-train_df <- select(train_df,-Yield)
-valid_df <- select(valid_df,-Yield)
-test_df <- select(test_df,-Yield)
-
+# Model Random Forest
 
 library(randomForest)
 
 set.seed(12321)
-rm1 <- randomForest(y = y_train, x = train_df, ntree = 10)
+rm1 <- randomForest(y = y_train, x = train_feat_df, ntree = 10)
 rmse(predict(rm1), y_train)
 rmse(predict(rm1, valid_df), y_valid)
 
 
 set.seed(123)
-rm2 <- randomForest(y = y_train, x = train_df, ntree = 500, verbose = T)
+rm2 <- randomForest(y = y_train, x = train_feat_df, ntree = 500, verbose = T)
 rmse(predict(rm2), y_train)
 rmse(predict(rm2, valid_df), y_valid)
 
